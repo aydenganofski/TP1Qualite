47bfb93594ff873d8b0f48ec86384e362e8da689
Ayden Ganofski <aydenganofski@gmail.com>
2026-09-11 16:19:38 +0200

    Modification de l'affichage

--- tp1git/src/main/java/iut/Main.java
+++ tp1git/src/main/java/iut/Main.java
@@ -2,7 +2,7 @@ package iut;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculatrice");
+        System.out.println("Addition fonctionnelle dans la calculatrice");
     }
 
 }
